e2c752da02e5d81f0bd68c039b022f292f601056
= <=>
2019-03-31 15:44:19 +0200

    FEAT(): knockoff popup echanges

--- Monopoly/Monopoly/Form_Board.Designer.cs
+++ Monopoly/Monopoly/Form_Board.Designer.cs
@@ -36,7 +36,6 @@
             this.textBox_commande = new System.Windows.Forms.TextBox();
             this.button_trade = new System.Windows.Forms.Button();
             this.button_end_turn = new System.Windows.Forms.Button();
-            this.button_mortage = new System.Windows.Forms.Button();
             this.button_roll_dice = new System.Windows.Forms.Button();
             this.listBox_console = new System.Windows.Forms.ListBox();
             this.drawPanel_plateau = new Monopoly.DrawPanel();
@@ -79,12 +78,11 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel_control.BackColor = System.Drawing.Color.Transparent;
+            this.panel_control.Controls.Add(this.button_roll_dice);
             this.panel_control.Controls.Add(this.button_out_of_jail);
             this.panel_control.Controls.Add(this.textBox_commande);
             this.panel_control.Controls.Add(this.button_trade);
             this.panel_control.Controls.Add(this.button_end_turn);
-            this.panel_control.Controls.Add(this.button_mortage);
-            this.panel_control.Controls.Add(this.button_roll_dice);
             this.panel_control.Controls.Add(this.listBox_console);
             this.panel_control.Location = new System.Drawing.Point(613, 12);
             this.panel_control.Name = "panel_control";
@@ -98,11 +96,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button_out_of_jail.BackColor = System.Drawing.SystemColors.Control;
             this.button_out_of_jail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_out_of_jail.Location = new System.Drawing.Point(3, 334);
+            this.button_out_of_jail.Location = new System.Drawing.Point(3, 308);
             this.button_out_of_jail.Name = "button_out_of_jail";
             this.button_out_of_jail.Size = new System.Drawing.Size(148, 23);
             this.button_out_of_jail.TabIndex = 5;
-            this.button_out_of_jail.Text = "GET OUT OF JAIL";
+            this.button_out_of_jail.Text = "SORTIR DE PRISON";
             this.button_out_of_jail.UseVisualStyleBackColor = false;
             this.button_out_of_jail.Click += new System.EventHandler(this.button_out_of_jail_Click);
             // 
@@ -112,64 +110,53 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox_commande.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox_commande.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_commande.Location = new System.Drawing.Point(3, 308);
+            this.textBox_commande.Location = new System.Drawing.Point(3, 283);
             this.textBox_commande.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
             this.textBox_commande.MinimumSize = new System.Drawing.Size(0, 20);
             this.textBox_commande.Name = "textBox_commande";
-            this.textBox_commande.Size = new System.Drawing.Size(148, 14);
+            this.textBox_commande.Size = new System.Drawing.Size(148, 20);
             this.textBox_commande.TabIndex = 7;
             this.textBox_commande.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_commande_KeyPress);
             // 
             // button_trade
             // 
-            this.button_trade.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_trade.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button_trade.BackColor = System.Drawing.SystemColors.Control;
-            this.button_trade.Enabled = false;
             this.button_trade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_trade.Location = new System.Drawing.Point(76, 363);
+            this.button_trade.Location = new System.Drawing.Point(3, 337);
             this.button_trade.Name = "button_trade";
-            this.button_trade.Size = new System.Drawing.Size(75, 23);
+            this.button_trade.Size = new System.Drawing.Size(148, 23);
             this.button_trade.TabIndex = 3;
-            this.button_trade.Text = "TRADE";
+            this.button_trade.Text = "ECHANGES";
             this.button_trade.UseVisualStyleBackColor = false;
             this.button_trade.Click += new System.EventHandler(this.button_trade_Click);
             // 
             // button_end_turn
             // 
-            this.button_end_turn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_end_turn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button_end_turn.BackColor = System.Drawing.SystemColors.Control;
             this.button_end_turn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_end_turn.Location = new System.Drawing.Point(3, 392);
+            this.button_end_turn.Location = new System.Drawing.Point(3, 395);
             this.button_end_turn.Name = "button_end_turn";
-            this.button_end_turn.Size = new System.Drawing.Size(85, 23);
+            this.button_end_turn.Size = new System.Drawing.Size(148, 23);
             this.button_end_turn.TabIndex = 4;
-            this.button_end_turn.Text = "END TURN";
+            this.button_end_turn.Text = "FIN DU TOUR";
             this.button_end_turn.UseVisualStyleBackColor = false;
             this.button_end_turn.Click += new System.EventHandler(this.button_end_turn_Click);
             // 
-            // button_mortage
-            // 
-            this.button_mortage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button_mortage.BackColor = System.Drawing.SystemColors.Control;
-            this.button_mortage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_mortage.Location = new System.Drawing.Point(3, 363);
-            this.button_mortage.Name = "button_mortage";
-            this.button_mortage.Size = new System.Drawing.Size(85, 23);
-            this.button_mortage.TabIndex = 2;
-            this.button_mortage.Text = "MORTGAGE";
-            this.button_mortage.UseVisualStyleBackColor = false;
-            this.button_mortage.Click += new System.EventHandler(this.button_mortage_Click);
-            // 
             // button_roll_dice
             // 
-            this.button_roll_dice.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_roll_dice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button_roll_dice.BackColor = System.Drawing.SystemColors.Control;
             this.button_roll_dice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_roll_dice.Location = new System.Drawing.Point(76, 392);
+            this.button_roll_dice.Location = new System.Drawing.Point(3, 366);
             this.button_roll_dice.Name = "button_roll_dice";
-            this.button_roll_dice.Size = new System.Drawing.Size(75, 23);
+            this.button_roll_dice.Size = new System.Drawing.Size(148, 23);
             this.button_roll_dice.TabIndex = 1;
-            this.button_roll_dice.Text = "ROLL DICE";
+            this.button_roll_dice.Text = "JETER LES DES";
             this.button_roll_dice.UseVisualStyleBackColor = false;
             this.button_roll_dice.Click += new System.EventHandler(this.button_roll_dice_Click);
             // 
@@ -182,7 +169,7 @@
             this.listBox_console.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listBox_console.Location = new System.Drawing.Point(3, 13);
             this.listBox_console.Name = "listBox_console";
-            this.listBox_console.Size = new System.Drawing.Size(148, 299);
+            this.listBox_console.Size = new System.Drawing.Size(148, 273);
             this.listBox_console.TabIndex = 6;
             // 
             // drawPanel_plateau
@@ -223,7 +210,6 @@
         #endregion
         private DrawPanel drawPanel_plateau;
         private System.Windows.Forms.Button button_roll_dice;
-        private System.Windows.Forms.Button button_mortage;
         private System.Windows.Forms.Button button_trade;
         private System.Windows.Forms.Button button_end_turn;
         private System.Windows.Forms.Button button_out_of_jail;
